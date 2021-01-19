5e1cfdba3f491b1956d27808544634169c61f3d4
BisiDavi <oludavidconnect@gmail.com>
2021-01-19 01:29:35 +0100

    payment api added

--- routes/merchantRoutes.js
+++ routes/merchantRoutes.js
@@ -1,27 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import {
-  authMerchant,
-  registerMerchant,
-  getMerchantProfile,
-  updateMerchantProfile,
-  getMerchants,
-  deleteMerchant,
-  getMerchantById,
-  updateMerchant
-} from '../controllers/merchantController.js';
+import { merchantSubscription } from '../controllers/merchantController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.route('/').post(registerMerchant).get(protect, admin, getMerchants);
-router.post('/login', authMerchant);
-router
-  .route('/profile')
-  .get(protect, getMerchantProfile)
-  .put(protect, updateMerchantProfile);
-router
-  .route('/:id')
-  .delete(protect, admin, deleteMerchant)
-  .get(protect, admin, getMerchantById)
-  .put(protect, admin, updateMerchant);
+router.route('/').post().get(protect, admin, merchantSubscription);
 
 export default router;
--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/merchant', merchantRoutes);
+app.use('/api/payment', merchantRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
 
