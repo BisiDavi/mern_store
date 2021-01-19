b7e07d1e0113aab28af39a92281641ff5b8b69ff
BisiDavi <oludavidconnect@gmail.com>
2021-01-19 01:38:03 +0100

    payment api

--- routes/merchantRoutes.js
+++ routes/merchantRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 import { merchantSubscription } from '../controllers/merchantController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.route('/').post().get(protect, admin, merchantSubscription);
+router.post('/', protect, admin, merchantSubscription);
 
 export default router;
